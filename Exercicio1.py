66de7d3579f25a61d73dbf692b774fa2f9b4b969
Fernando <fernando22future@gmail.com>
2020-04-17 19:45:14 -0300

    Atualização

--- Exercicio1.py
+++ Exercicio1.py
@@ -7,17 +7,45 @@ sales = pd.read_csv(
     parse_dates=['Date'])
 
 sales.head()
+
+var = sales.shape
+
+sales.info()
+
+sales.describe()
+
+sales['Unit_Cost'].describe()
+
+sales['Unit_Cost'].mean()
+
+sales['Unit_Cost'].median()
+sales['Unit_Cost'].plot(kind='box', vert=False, figsize=(14, 6))
+# sales['Unit_Cost'].plot(kind='density', figsize=(14, 6))
+
+ax = sales['Unit_Cost'].plot(kind='box', figsize=(14, 6))
+ax.axvline(sales['Unit_Cost'].mean(), color='red')
+ax.axvline(sales['Unit_Cost'].median(), color='green')
+
+ax = sales['Unit_Cost'].plot(kind='hist', figsize=(14, 6))
+ax.set_ylabel('Number of Sales')
+ax.set_xlabel('dollars')
+
 sales['Customer_Age'].mean()
 sales['Customer_Age'].plot(kind='box', vert=False, figsize=(14, 6))
+
 sales['Year'].value_counts()
 sales['Year'].value_counts().plot(kind='bar', figsize=(6, 6))
+
 sales['Month'].value_counts()
 sales['Month'].value_counts().plot(kind='bar', figsize=(14, 6))
+
 sales['Country'].value_counts().head(1)
 sales['Country'].value_counts()
 sales['Country'].value_counts().plot(kind='bar', figsize=(14, 6))
+
 sales['Product'].unique()
 sales['Product'].value_counts().head(10).plot(kind='bar', figsize=(14, 6))
+
 sales.plot(kind='scatter', x='Unit_Cost', y='Unit_Price', figsize=(6, 6))
 sales.plot(kind='scatter', x='Order_Quantity', y='Profit', figsize=(6, 6))
 sales[['Profit', 'Country']].boxplot(by='Country', figsize=(10, 6))
@@ -25,11 +53,21 @@ sales[['Customer_Age', 'Country']].boxplot(by='Country', figsize=(10, 6))
 sales['Calculated_Date'] = sales[['Year', 'Month', 'Day']].apply(lambda x: '{}-{}-{}'.format(x[0], x[1], x[2]), axis=1)
 
 sales['Calculated_Date'].head()
-
 sales['Calculated_Date'] = pd.to_datetime(sales['Calculated_Date'])
-
 sales['Calculated_Date'].head()
 sales['Calculated_Date'].value_counts().plot(kind='line', figsize=(14, 6))
+
+corr = sales.corr()
+
+fig = plt.figure(figsize=(8, 8))
+plt.matshow(corr, cmap='RdBu', fignum=fig.number)
+plt.xticks(range(len(corr.columns)), corr.columns, rotation='vertical')
+plt.yticks(range(len(corr.columns)), corr.columns)
+
+sales.plot(kind='scatter', x='Customer_Age', y='Revenue', figsize=(6, 6))
+
+sales.plot(kind='scatter', x='Revenue', y='Profit', figsize=(6, 6))
+
 sales['Revenue'] += 50
 var = sales.loc[(sales['Country'] == 'Canada') | (sales['Country'] == 'France')].shape[0]
 var = sales.loc[(sales['Country'] == 'Canada') & (sales['Sub_Category'] == 'Bike Racks')].shape[0]
